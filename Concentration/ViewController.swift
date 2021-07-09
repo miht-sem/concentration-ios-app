f6ba45542027e7e634efecc0a982e664a30889a1
Miht.sem <miht.sem@yandex.com>
2021-07-09 14:40:08 +0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Семен Михтанюк on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
